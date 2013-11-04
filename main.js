9459ffccacdc41ee660bde6647d71e5c65a13f31
MSakamaki <daigouzigai5@gmail.com>
2013-11-04 15:28:52 +0900

    add send event

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 
-var  server = require('websocket.io').listen(80, function(){
+var  server = require('websocket.io').listen(8080, function(){
 	console.log('server start');
 });
 
 server.on('connection', function(socket){
-	console.log('connect!', socket);
+	//console.log('connect!', socket);
 	socket.on('message', function(event){
 		console.log('message', event);
+		var data = JSON.parse(event);
+		server.clients.forEach(function(client){
+			if(client){
+				client.send(JSON.stringify({ data : 'hello websocket!' }));
+			}
+		});
 	});
 });
 
